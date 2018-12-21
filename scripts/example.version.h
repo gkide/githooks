f438f127094e2e911c2dbe9b4b8d82173d8059b0
Charlie WONG <1213charlie@163.com>
2018-12-21 22:20:17 +0800

    style: file rename only

--- scripts/example.version.h
+++ scripts/example.version.h
@@ -4,6 +4,9 @@
 #define XXX_VERSION_MAJOR 1
 #define XXX_VERSION_MINOR 2
 #define XXX_VERSION_PATCH 3
+
+// The prefix can not contains " or '
+// " and ' will be auto skiped
 #define XXX_VERSION_TWEAK "pre+20181221"
 
 #endif // BUILD_CONFIG_VERSION_H
