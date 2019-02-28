ae31d90f719ba8d4f9bb83993ece0e79009cbda6
Charlie WONG <1213charlie@163.com>
2019-03-01 04:30:43 +0800

    feat: release scripts to bump semver & changelog

---
+++ scripts/user/git/SemVer.c1.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# NOTE:
+#
+# 1. User Name & Email Must be consist of [0-9A-Za-z@.- ]
+#    The format is: USER_NAME <USER_EMAIL>
+#    - GIT: USER_NAME/USER_EMAIL the same as 'git config' if not set
+#    - SVN: USER_NAME auto get from 'svn log', USER_EMAIL is empty by default
+#    if not set, then user name & email will auto detected as above
+#    USER_NAME="my name"
+#    USER_EMAIL="email@my.com"
+# 2. This current file & ${VS_VFILE} should be files of the git repo
+
+# Repo root directory, full path
+REPO_VCS="GIT";
+REPO_DIR="$(git rev-parse --show-toplevel)";
+
+# Repo info file to sync
+VS_VFILE="${REPO_DIR}/scripts/user/SemVer.c1";
+
+# Semantic version
+# Version should consist of MAJOR/MINOR/PATCH/TWEAK
+#   The MAJOR, MINOR, PATCH must consit of numbers of [0-9]
+#   TWEAK is pre-release part, consist of [a-z0-9.-]
+# Each line must has one, like the following
+#   MAJOR   Can not ignore, must be 0 ~ 9
+#   MINOR   Can not ignore, must be 0 ~ 9
+#   PATCH   Can not ignore, must be 0 ~ 9
+#   TWEAK   Can be ignored, consist of [a-z0-9.-]
+
+# NOTE
+# The following value's special chars will be escaped or replaced
+# - space     \s+
+# - ()        \(
+# - *         \*
+# - [         \[
+# - ]         \]
+
+VS_MAJOR="static const char *semver_major =";
+VS_MINOR="static const char *semver_minor =";
+VS_PATCH="static const char *semver_patch =";
+VS_TWEAK="static const char *semver_tweak =";
