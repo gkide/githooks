28f1cce54d73699937c4efda9b49ab60a4fb44ab
Charlie WONG <1213charlie@163.com>
2019-04-19 03:45:04 +0800

    fix: repo submodule commit message checking

--- policy/check-msg-fmt.sh
+++ policy/check-msg-fmt.sh
@@ -3,6 +3,13 @@
 GIT_REPO_DIR="$(git rev-parse --show-toplevel)"
 
 # https://www.npmjs.com/package/@gkide/standard-release
-standard-release -x -m "${GIT_REPO_DIR}/${1}"
+if [[ ${1} == */.git/modules/* ]]; then
+    standard-release -x -m "${1}" # submodule commit
+    # modulePath="$(dirname ${1})"
+    # moduleName="$(basename ${modulePath})"
+    # echo "update module ${moduleName}"
+else
+    standard-release -x -m "${GIT_REPO_DIR}/${1}"
+fi
 
 exit $?
